25527c2712a4e6d1ccb3d3d2e3e532d6d46c9fc4
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2023-03-19 16:25:42 -0300

    feat: conect button to send notification

--- frontend/src/components/NotificationButton/NotificationButton.tsx
+++ frontend/src/components/NotificationButton/NotificationButton.tsx
@@ -1,9 +1,22 @@
 import './style.css'
 import notificationIcon from '../../assets/img/notification-icon.svg'
+import axios from 'axios';
+import { BASE_URL } from '../../utils/request';
 
-export function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(saleId : number) {
+    axios(`${BASE_URL}/sales/${saleId}/notification`)
+    .then(response => {
+        console.log("SUCESSO")
+    })
+}
+
+export function NotificationButton( {saleId} : Props ) {
     return (
-        <div className="dsmeta-red-btn-container">
+        <div className="dsmeta-red-btn-container" onClick={() => handleClick(saleId)}>
             <div className="dsmeta-red-btn">
                 <img src={notificationIcon} alt="Notificar" />
             </div>
